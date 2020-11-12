425aa3cb88b111bb3f4f8060b19ee59b87003174
Nicholas Hammond <hammondn649@gmail.com>
2020-11-12 16:05:04 +1100

    add javascript file, add another media query to keep items fixed length, position home page subtitle

---
+++ code.js
@@ -0,0 +1,29 @@
+
+
+
+// console.log(document.getElementById("resume-circle-top").getAttribute("r"));
+
+// console.log(document.getElementById("resume-svg").getAttribute("width"));
+
+
+
+// document.getElementById("resume-circle-top").setAttribute("r" ,10);
+
+
+// document.getElementById("resume-svg").addEventListener("mouseover", function(){
+//     document.getElementById("resume-circle-top").setAttribute("r" ,48);
+//     document.getElementById("resume-svg").setAttribute("width" ,500);
+//     document.getElementById("resume-svg").setAttribute("height" ,500);
+//     document.getElementById("resume-button").setAttribute("style" ,"height:100%");
+// });
+
+// document.getElementById("resume-svg").addEventListener("mouseleave", function(){
+//     document.getElementById("resume-circle-top").setAttribute("r" ,0);
+//     document.getElementById("resume-svg").setAttribute("width" ,100);
+//     document.getElementById("resume-svg").setAttribute("height" ,100);
+//     document.getElementById("resume-button").setAttribute("style" ,"height:100px");
+// });
+
+console.log(document.getElementById("background-path1"));
+
+document.getElementById("background-path1").setAttribute("d", "M0 50 0 100 100 100  50 50Z");
