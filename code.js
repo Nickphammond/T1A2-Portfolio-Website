42c821dde232f530edd021f149cb561cfe147926
Nicholas Hammond <hammondn649@gmail.com>
2020-11-14 12:07:51 +1100

    finish javascript branch

--- code.js
+++ code.js
@@ -9,21 +9,28 @@
 
 // document.getElementById("resume-circle-top").setAttribute("r" ,10);
 
+if(document.getElementsByTagName("body")[0].id == "about") {
+    document.getElementById("resume-svg").addEventListener("mouseover", function(){
+        document.getElementById("resume-circle-top").setAttribute("r" ,48);
+        document.getElementById("resume-svg").setAttribute("width" ,500);
+        document.getElementById("resume-svg").setAttribute("height" ,500);
+        document.getElementById("resume-button").setAttribute("style" ,"height:100%");
+    });
+    
+    document.getElementById("resume-svg").addEventListener("mouseleave", function(){
+        document.getElementById("resume-circle-top").setAttribute("r" ,0);
+        document.getElementById("resume-svg").setAttribute("width" ,100);
+        document.getElementById("resume-svg").setAttribute("height" ,100);
+        document.getElementById("resume-button").setAttribute("style" ,"height:100px");
+    });
+} else {
+    document.getElementById("background-path1").setAttribute("d", "M-50 50 -50 100 100 100  C 50 100, 40 80, -50 50Z");
+};
 
-// document.getElementById("resume-svg").addEventListener("mouseover", function(){
-//     document.getElementById("resume-circle-top").setAttribute("r" ,48);
-//     document.getElementById("resume-svg").setAttribute("width" ,500);
-//     document.getElementById("resume-svg").setAttribute("height" ,500);
-//     document.getElementById("resume-button").setAttribute("style" ,"height:100%");
-// });
 
-// document.getElementById("resume-svg").addEventListener("mouseleave", function(){
-//     document.getElementById("resume-circle-top").setAttribute("r" ,0);
-//     document.getElementById("resume-svg").setAttribute("width" ,100);
-//     document.getElementById("resume-svg").setAttribute("height" ,100);
-//     document.getElementById("resume-button").setAttribute("style" ,"height:100px");
-// });
 
-console.log(document.getElementById("background-path1"));
 
-document.getElementById("background-path1").setAttribute("d", "M0 50 0 100 100 100  50 50Z");
+
+
+// document.getElementById("background-path1").setAttribute("d", "M0 50 0 100 100 100Z");
+
